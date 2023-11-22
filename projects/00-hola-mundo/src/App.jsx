e85f83b0c5cc0202299b60182fc763936d5fa738
Alexander Rengifo Cardona <you@example.com>
2023-11-22 18:31:53 -0500

    chore(Root): setup project

---
+++ projects/00-hola-mundo/src/App.jsx
@@ -0,0 +1,43 @@
+import { TwitterFollowCard } from "./TwitterFollowCard"
+import "./App.css"
+
+const users = [
+    {
+      userName: 'midudev',
+      name: 'Miguel Ángel Durán',
+      isFollowing: true
+    },
+    {
+      userName: 'pheralb',
+      name: 'Pablo H.',
+      isFollowing: false
+    },
+    {
+      userName: 'AlexRenCar',
+      name: 'Alexander Rengifo Cardona',
+      isFollowing: true
+    },
+    {
+      userName: 'TMChein',
+      name: 'Tomas',
+      isFollowing: false
+    }
+  ]
+
+export function App () {
+    return(
+        <section className="App">
+            {
+                users.map(({ userName, name, isFollowing }) => (
+                    <TwitterFollowCard
+                        key={userName}
+                        userName={userName}
+                        initialIsFollowing={isFollowing}
+                    >
+                        {name}
+                    </TwitterFollowCard>
+                ))
+            }
+        </section>
+    )
+}
